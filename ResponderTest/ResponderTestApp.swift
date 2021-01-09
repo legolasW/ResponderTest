fd988115e7c492aab84be62bb96dc0a1a8901a02
Legolas <057@live.com>
2021-01-08 16:19:51 -0800

    Initial Commit

---
+++ ResponderTest/ResponderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponderTestApp.swift
+//  ResponderTest
+//
+//  Created by Legolas on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct ResponderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
